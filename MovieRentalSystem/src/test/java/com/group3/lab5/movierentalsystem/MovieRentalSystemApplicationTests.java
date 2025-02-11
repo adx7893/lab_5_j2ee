8a8879f6c339cd4e1a9705519b996cf004418dbe
sarathchandran karintha <sarathck84@gmail.com>
2025-02-11 13:46:50 -0500

    added project structure

---
+++ MovieRentalSystem/src/test/java/com/group3/lab5/movierentalsystem/MovieRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.lab5.movierentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
