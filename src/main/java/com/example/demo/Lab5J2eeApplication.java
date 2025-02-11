6d9e29c3b9f3fe646db96b34146319ba800c250c
“Apurva <apurvakansara5.com>
2025-02-11 13:54:06 -0500

    updated_code

---
+++ src/main/java/com/example/demo/Lab5J2eeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5J2eeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab5J2eeApplication.class, args);
+	}
+
+}
